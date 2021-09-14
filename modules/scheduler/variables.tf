32fece305b3e6f355932282e42526add65318e35
LeonIrv <89283865+LeonIrv@users.noreply.github.com>
2021-09-14 16:01:37 -0500

    Update variables.tf

--- modules/scheduler/variables.tf
+++ modules/scheduler/variables.tf
@@ -14,10 +14,6 @@ variable "time_zone" {
   type        = string
   description = "Time Zone to be used in interpreting schedule"
 }
-variable "base_value" {
-  type        = string
-  description = "Base 64 encoded-non-empty string, per requirements"
-}
 variable "topic_name" {
   type        = string
   description = "Pub/Sub Topic Name"
