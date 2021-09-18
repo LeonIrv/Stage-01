58754232ebc8633404321de80f13c8840f8ea3d4
LeonIrv <89283865+LeonIrv@users.noreply.github.com>
2021-09-18 00:10:09 -0500

    Update gcs.tf

--- modules/bucket/gcs.tf
+++ modules/bucket/gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "PubsubBucket" {
-  name                        = "epam-demo-bucket"
+  name                        = "epam-demo-bucket-01"
   location                    = var.gcp_region
   project                     = var.project_id
   uniform_bucket_level_access = var.uniform_bucket_level_access
